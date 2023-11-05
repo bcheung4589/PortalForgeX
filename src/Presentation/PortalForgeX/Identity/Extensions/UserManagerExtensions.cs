3d63bf45f3d7413ca9c8d116a1f0f667f004926f
Bryan Cheung <bryan360@live.nl>
2023-11-05 14:35:00 +0100

    impersonation

--- src/Presentation/PortalForgeX/Identity/Extensions/UserManagerExtensions.cs
+++ src/Presentation/PortalForgeX/Identity/Extensions/UserManagerExtensions.cs
@@ -1,10 +1,40 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using PortalForgeX.Domain.Entities.Identity;
+using PortalForgeX.Infrastructure.Tenants;
+using System.Security.Claims;
 
 namespace PortalForgeX.Identity.Extensions;
 
 public static class UserManagerExtensions
 {
+    public static async Task RegisterSucceededLoginAsync(this UserManager<ApplicationUser> source, string userName)
+    {
+        var user = await source.FindByNameAsync(userName);
+        if (user == null)
+        {
+            return;
+        }
+
+        // Assign the TenantId to the User Claims
+        if (user.TenantId is not null && user.TenantId != Guid.Empty)
+        {
+            var userClaims = await source.GetClaimsAsync(user);
+            if (userClaims.Any())
+            {
+                var tenantClaims = userClaims.Where(x => x.Type == TenantClaimTypes.TenantId);
+                if (tenantClaims.Any())
+                {
+                    await source.RemoveClaimsAsync(user, tenantClaims);
+                }
+            }
+
+            await source.AddClaimAsync(user, new Claim(TenantClaimTypes.TenantId, user.TenantId.ToString()!));
+        }
+
+        user.LastLoggedInTime = DateTime.UtcNow;
+        _ = await source.UpdateAsync(user);
+    }
+
     public static async Task<IdentityResult> UpdateRolesAsync(this UserManager<ApplicationUser> source, ApplicationUser user, IDictionary<ApplicationRole, bool> userRoles)
     {
         var updatedRole = false;
