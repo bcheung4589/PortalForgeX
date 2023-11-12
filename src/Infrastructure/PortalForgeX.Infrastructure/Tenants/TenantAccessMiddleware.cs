05670354e59521d3bf66aac1073f6936d60968fe
Bryan Cheung <bryan360@live.nl>
2023-11-12 03:26:45 +0100

    create usergroup

--- src/Infrastructure/PortalForgeX.Infrastructure/Tenants/TenantAccessMiddleware.cs
+++ src/Infrastructure/PortalForgeX.Infrastructure/Tenants/TenantAccessMiddleware.cs
@@ -3,6 +3,10 @@ using PortalForgeX.Application.Tenants;
 
 namespace PortalForgeX.Infrastructure.Tenants;
 
+/// <summary>
+/// Scan the HttpContext for potential Tenant. If found, load it into the Accessor.
+/// </summary>
+/// <param name="tenantAccessor"></param>
 public class TenantAccessMiddleware(TenantAccessor tenantAccessor) : IMiddleware
 {
     private readonly TenantAccessor _tenantAccessor = tenantAccessor;
---
+++ src/Presentation/PortalForgeX/Extensions/TenantAccessorExtensions.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
+using PortalForgeX.Application.Tenants;
+
+namespace PortalForgeX.Extensions;
+
+public static class TenantAccessorExtensions
+{
+    /// <summary>
+    /// Load the Tenant; if none found, navigate to dashboard.
+    /// </summary>
+    /// <param name="source"></param>
+    /// <param name="authenticationStateProvider"></param>
+    /// <param name="navigation"></param>
+    /// <returns></returns>
+    public static async Task RequireTenantAsync(this TenantAccessor source, AuthenticationStateProvider authenticationStateProvider, NavigationManager navigation)
+    {
+        await source.LoadFromAsync((await authenticationStateProvider.GetAuthenticationStateAsync()).User);
+
+        if (!source.HasTenant)
+        {
+            navigation.NavigateTo("/");
+        }
+    }
+}
